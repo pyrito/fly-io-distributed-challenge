188737f421ac88569a2b18ddf210c23652d42262
Karthik Velayutham <vkarthik@ponder.io>
2023-04-03 23:01:20 -0500

    Struggling out here

--- challenge_4-grow-only-counter/counter.go
+++ challenge_4-grow-only-counter/counter.go
@@ -4,39 +4,27 @@ import (
 	"context"
 	"encoding/json"
 	"log"
-	"sync"
 	"time"
 
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
 
+const (
+	count_key = "count_key"
+)
+
 type Service struct {
 	node *maelstrom.Node
 	kv   *maelstrom.KV
-
-	kvLock *sync.RWMutex
 }
 
 func NewService(node *maelstrom.Node, kv *maelstrom.KV) *Service {
 	service := &Service{
-		node:   node,
-		kv:     kv,
-		kvLock: &sync.RWMutex{},
+		node: node,
+		kv:   kv,
 	}
 
 	return service
-}
-
-func (s *Service) InitHandler(msg maelstrom.Message) error {
-	// By this point the Node should have initialized some stuff
-	ctx_write, cancel_write := context.WithTimeout(context.Background(), time.Second)
-	defer cancel_write()
-
-	if err := s.kv.Write(ctx_write, s.node.ID(), 0); err != nil {
-		log.Println(err)
-	}
-
-	return nil
 }
 
 func (s *Service) AddHandler(msg maelstrom.Message) error {
@@ -45,23 +33,28 @@ func (s *Service) AddHandler(msg maelstrom.Message) error {
 		return err
 	}
 
-	s.kvLock.Lock()
 	delta := int(body["delta"].(float64))
 	ctx_read, cancel_read := context.WithTimeout(context.Background(), time.Second)
 	defer cancel_read()
 
-	old_val, err := s.kv.ReadInt(ctx_read, s.node.ID())
+	old_val, err := s.kv.ReadInt(ctx_read, count_key)
 	if err != nil {
-		return err
+		rpcError, ok := err.(*maelstrom.RPCError)
+		// If the key doesn't exist, let's initialize it
+		if ok && rpcError.Code == maelstrom.KeyDoesNotExist {
+			old_val = 0
+		} else {
+			return err
+		}
 	}
 
 	// First read the latest value, then add it with the delta and write it out
 	ctx_write, cancel_write := context.WithTimeout(context.Background(), time.Second)
 	defer cancel_write()
-	if err := s.kv.Write(ctx_write, s.node.ID(), old_val+delta); err != nil {
+
+	if err := s.kv.CompareAndSwap(ctx_write, count_key, old_val, old_val+delta, true); err != nil {
 		return err
 	}
-	s.kvLock.Unlock()
 
 	return s.node.Reply(msg, map[string]any{
 		"type": "add_ok",
@@ -72,7 +65,7 @@ func (s *Service) ReadHandler(msg maelstrom.Message) error {
 	ctx_read, cancel_read := context.WithTimeout(context.Background(), time.Second)
 	defer cancel_read()
 
-	value, err := s.kv.ReadInt(ctx_read, s.node.ID())
+	value, err := s.kv.ReadInt(ctx_read, count_key)
 	if err != nil {
 		return err
 	}
@@ -82,14 +75,15 @@ func (s *Service) ReadHandler(msg maelstrom.Message) error {
 	})
 }
 
+// What does sequential consistency mean - it means that we should never view
+// a state from a previous time. If W(x, 1) happens before W(x, 2), then if we
+// read W(x, 2)
 func main() {
 	n := maelstrom.NewNode()
 	kv := maelstrom.NewSeqKV(n)
 
 	nodeService := NewService(n, kv)
 
-	// I learned the hard way that the init handler needs to be set
-	n.Handle("init", nodeService.InitHandler)
 	n.Handle("add", nodeService.AddHandler)
 	n.Handle("read", nodeService.ReadHandler)
 
